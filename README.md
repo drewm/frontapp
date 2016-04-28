2061c6c6fca541ad30cbd765d7f2c5d72bcc6111
drewm <drew.mclellan@gmail.com>
2016-04-28 15:21:23 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# frontapp
-PHP client library for Front API
+FrontApp API client
+===================
+
+Super-simple, minimum abstraction FrontApp API wrapper, in PHP.
+
+In the style of my [MailChimp wrapper](https://github.com/drewm/mailchimp-api), this lets you get from the FrontApp API docs to the code as directly as possible.
+
+Requires PHP 5.4.
+
+[![Build Status](https://travis-ci.org/drewm/frontapp.svg?branch=master)](https://travis-ci.org/drewm/frontapp)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/drewm/frontapp/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/drewm/frontapp/?branch=master)
+[![Packagist](https://img.shields.io/packagist/dt/drewm/frontapp.svg?maxAge=2592000)](https://packagist.org/packages/drewm/frontapp)
+
+Installation
+------------
+
+You can install frontapp using Composer:
+
+```
+composer require drewm/frontapp
+```
+
+You will then need to:
+* run ``composer install`` to get these dependencies added to your vendor directory
+* add the autoloader to your application with this line: ``require("vendor/autoload.php")``
+
+Examples
+--------
+
+Start by `use`-ing the class and creating an instance with your API key
+
+```php
+use \DrewM\FrontApp\FrontApp;
+
+$FrontApp = new FrontApp('abc123abc123abc123abc123abc123');
+```
+
+Then, list all your inboxes (with a `get` on the `inboxes` method)
+
+```php
+$result = $FrontApp->get('inboxes');
+
+print_r($result);
+```
+
+Create an inbox (with a `post` to the `inboxes` method):
+
+```php
+$list_id = 'b1234346';
+
+$result = $FrontApp->post("inboxes", [
+				'name' => 'Support'
+			]);
+
+print_r($result);
+```
+
+Update a channel member with more information (using `patch` to update):
+
+```php
+$channel_id = 'cha_123';
+
+$result = $FrontApp->patch("channels/$channel_id", [
+				'settings' => [ 'webhook_url' => 'https://example.com/hook' ]
+			]);
+
+print_r($result);
+```
+
+Remove a contact using the `delete` method:
+
+```php
+$contact_id = '1234';
+
+$FrontApp->delete("contacts/$contact_id");
+```
+
+Quickly test for a successful action with the `success()` method:
+
+```php
+$result = $FrontApp->get('inboxes');
+
+if ($FrontApp->success()) {
+	print_r($result);	
+} else {
+	echo $FrontApp->getLastError();
+}
+```
+
+Troubleshooting
+---------------
+
+To get the last error returned by either the HTTP client or by the API, use `getLastError()`:
+
+```php
+echo $FrontApp->getLastError();
+```
+
+For further debugging, you can inspect the headers and body of the response:
+
+```php
+print_r($FrontApp->getLastResponse());
+```
+
+If you suspect you're sending data in the wrong format, you can look at what was sent to FrontApp by the wrapper:
+
+```php
+print_r($FrontApp->getLastRequest());
+```
---
+++ src/FrontApp.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * Super-simple, minimum abstraction FrontApp API wrapper
+ * This wrapper: https://github.com/drewm/frontapp
+ *
+ * @author Drew McLellan <drew.mclellan@gmail.com>
+ * @version 0.1
+ */
+class FrontApp
+{
+    private $api_key;
+    private $api_endpoint = 'https://api2.frontapp.com';
+
+    /*  SSL Verification
+        Read before disabling: 
+        http://snippets.webaware.com.au/howto/stop-turning-off-curlopt_ssl_verifypeer-and-fix-your-php-config/
+    */
+    public $verify_ssl = true;
+
+    private $request_successful = false;
+    private $last_error         = '';
+    private $last_response      = array();
+    private $last_request       = array();
+
+    /**
+     * Create a new instance
+     * @param string $api_key Your FrontApp API key
+     * @throws \Exception
+     */
+    public function __construct($api_key)
+    {
+        $this->api_key = $api_key;
+        $this->last_response = array('headers' => null, 'body' => null);
+    }
+
+    /**
+     * Was the last request successful?
+     * @return bool  True for success, false for failure
+     */
+    public function success()
+    {
+        return $this->request_successful;
+    }
+
+    /**
+     * Get the last error returned by either the network transport, or by the API.
+     * If something didn't work, this should contain the string describing the problem.
+     * @return  array|false  describing the error
+     */
+    public function getLastError()
+    {
+        return $this->last_error ?: false;
+    }
+
+    /**
+     * Get an array containing the HTTP headers and the body of the API response.
+     * @return array  Assoc array with keys 'headers' and 'body'
+     */
+    public function getLastResponse()
+    {
+        return $this->last_response;
+    }
+
+    /**
+     * Get an array containing the HTTP headers and the body of the API request.
+     * @return array  Assoc array
+     */
+    public function getLastRequest()
+    {
+        return $this->last_request;
+    }
+
+    /**
+     * Make an HTTP DELETE request - for deleting data
+     * @param   string $method URL of the API request method
+     * @param   array $args Assoc array of arguments (if any)
+     * @param   int $timeout Timeout limit for request in seconds
+     * @return  array|false   Assoc array of API response, decoded from JSON
+     */
+    public function delete($method, $args = array(), $timeout = 10)
+    {
+        return $this->makeRequest('delete', $method, $args, $timeout);
+    }
+
+    /**
+     * Make an HTTP GET request - for retrieving data
+     * @param   string $method URL of the API request method
+     * @param   array $args Assoc array of arguments (usually your data)
+     * @param   int $timeout Timeout limit for request in seconds
+     * @return  array|false   Assoc array of API response, decoded from JSON
+     */
+    public function get($method, $args = array(), $timeout = 10)
+    {
+        return $this->makeRequest('get', $method, $args, $timeout);
+    }
+
+    /**
+     * Make an HTTP PATCH request - for performing partial updates
+     * @param   string $method URL of the API request method
+     * @param   array $args Assoc array of arguments (usually your data)
+     * @param   int $timeout Timeout limit for request in seconds
+     * @return  array|false   Assoc array of API response, decoded from JSON
+     */
+    public function patch($method, $args = array(), $timeout = 10)
+    {
+        return $this->makeRequest('patch', $method, $args, $timeout);
+    }
+
+    /**
+     * Make an HTTP POST request - for creating and updating items
+     * @param   string $method URL of the API request method
+     * @param   array $args Assoc array of arguments (usually your data)
+     * @param   int $timeout Timeout limit for request in seconds
+     * @return  array|false   Assoc array of API response, decoded from JSON
+     */
+    public function post($method, $args = array(), $timeout = 10)
+    {
+        return $this->makeRequest('post', $method, $args, $timeout);
+    }
+
+    /**
+     * Make an HTTP PUT request - for creating new items
+     * @param   string $method URL of the API request method
+     * @param   array $args Assoc array of arguments (usually your data)
+     * @param   int $timeout Timeout limit for request in seconds
+     * @return  array|false   Assoc array of API response, decoded from JSON
+     */
+    public function put($method, $args = array(), $timeout = 10)
+    {
+        return $this->makeRequest('put', $method, $args, $timeout);
+    }
+
+    /**
+     * Performs the underlying HTTP request. Not very exciting.
+     * @param  string $http_verb The HTTP verb to use: get, post, put, patch, delete
+     * @param  string $method The API method to be called
+     * @param  array $args Assoc array of parameters to be passed
+     * @param int $timeout
+     * @return array|false Assoc array of decoded result
+     * @throws \Exception
+     */
+    private function makeRequest($http_verb, $method, $args = array(), $timeout = 10)
+    {
+        if (!function_exists('curl_init') || !function_exists('curl_setopt')) {
+            throw new \Exception("cURL support is required, but can't be found.");
+        }
+
+        $url = $this->api_endpoint . '/' . $method;
+
+        $this->last_error         = '';
+        $this->request_successful = false;
+        $response                 = array('headers' => null, 'body' => null);
+        $this->last_response      = $response;
+
+        $this->last_request = array(
+            'method'  => $http_verb,
+            'path'    => $method,
+            'url'     => $url,
+            'body'    => '',
+            'timeout' => $timeout,
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $this->api_key
+        ));
+        curl_setopt($ch, CURLOPT_USERAGENT, 'DrewM/FrontApp/0.1 (github.com/drewm/frontapp)');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->verify_ssl);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($ch, CURLOPT_ENCODING, '');
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+
+        switch ($http_verb) {
+            case 'post':
+                curl_setopt($ch, CURLOPT_POST, true);
+                $this->attachRequestPayload($ch, $args);
+                break;
+
+            case 'get':
+                $query = http_build_query($args);
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . $query);
+                break;
+
+            case 'delete':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                break;
+
+            case 'patch':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+                $this->attachRequestPayload($ch, $args);
+                break;
+
+            case 'put':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                $this->attachRequestPayload($ch, $args);
+                break;
+        }
+
+        $response['body']    = curl_exec($ch);
+        $response['headers'] = curl_getinfo($ch);
+
+        if (isset($response['headers']['request_header'])) {
+            $this->last_request['headers'] = $response['headers']['request_header'];
+        }
+
+        if ($response['body'] === false) {
+            $this->last_error = curl_error($ch);
+        }
+
+        curl_close($ch);
+
+        return $this->formatResponse($response);
+    }
+
+    /**
+     * Encode the data and attach it to the request
+     * @param   resource $ch cURL session handle, used by reference
+     * @param   array $data Assoc array of data to attach
+     */
+    private function attachRequestPayload(&$ch, $data)
+    {
+        $encoded = json_encode($data);
+        $this->last_request['body'] = $encoded;
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded);
+    }
+
+    /**
+     * Decode the response and format any error messages for debugging
+     * @param array $response The response from the curl request
+     * @return array|false     The JSON decoded into an array
+     */
+    private function formatResponse($response)
+    {
+        $this->last_response = $response;
+
+        if (!empty($response['body'])) {
+
+            $d = json_decode($response['body'], true);
+
+            if (isset($d['status']) && $d['status'] != '200' && isset($d['detail'])) {
+                $this->last_error = sprintf('%d: %s', $d['status'], $d['detail']);
+            } else {
+                $this->request_successful = true;
+            }
+
+            return $d;
+        }
+
+        return false;
+    }
+}
